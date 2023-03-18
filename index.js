4c044b26c2ec369e2bfd79e4535cb88008422906
Sam Galindo <ovosamgalindo@gmail.com>
2023-03-17 17:05:04 -0700

    passed all tests

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  // Write your code here! 
+  let message
+  if (feet <= 400) {
+    message = `This one is on me!`
+    return message
+  }
+  else if (feet > 400 && feet <= 2000 ){
+    message = `That will be twenty bucks.`
+    return message 
+  }
+  else if (feet > 2000 && feet <= 2500) {
+    message = `I will gladly take your thirty bucks.`
+    return message 
+  }
+  else message = `No can do.`
+  return message
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let message;
+  switch(tip){
+    case `generous`:
+      message = `Thank you so much.`
+      break;
+    case `not as generous`:
+      message = `Thank you.`
+      break;
+    default: 
+    message = `Bye.`
+  }
+  return message 
 }
